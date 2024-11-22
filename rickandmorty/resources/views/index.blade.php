2a1d8fd96b067c75d8d04592ad432d9c46b9611d
Leonardo Augusto-CASA <leonardodevaugusto2d@gmail.com>
2024-11-22 09:46:29 -0300

    Incluindo Versionamento

--- rickandmorty/resources/views/index.blade.php
+++ rickandmorty/resources/views/index.blade.php
@@ -117,7 +117,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
     <footer class="bg-dark text-white text-center py-3">
-        <p>&copy; 2024 Rick and Morty App. Todos os direitos reservados.</p>
+        <p>&copy; Copyright © 2024 Rick and Morty App. Todos os direitos reservados.powered by Leonardo Augusto - v1.0.0</p>
     </footer>
 
 </body>
